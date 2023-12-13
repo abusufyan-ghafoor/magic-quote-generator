d1fb6d35b1ea5d7407c6813c35a9d53dee352ac6
abusufyan-ghafoor <abusufyan.ghafoor44@gmail.com>
2023-12-13 23:22:04 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> Hello This is the first app</header>
+    </div>
+  )
+}
+
+export default App
